6075c843ac1ad95be684fa6666092fa4dcc71458
vaishali <vaishali@Ashoks-Air.attlocal.net>
2021-08-10 16:41:51 -0500

    adding file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,150 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Navigation from './Components/Navigation/Navigation';
+import SignIn from './Components/SignIn/SignIn';
+import Register from './Components/Register/Register';
+import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
+import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+const particlesOptions = {
+  particles: {
+    number:{
+      value:90,
+      density:{
+        enable: true,
+        value_area:900
+      }
+    }
+  }
 }
 
+const initialState = {
+      input: '',
+      imageUrl:'',
+      box: {},
+      route: 'signin',
+      isSignedIn: false,
+      user: {
+        id:'',
+        name: '',
+        email:'',
+        entries:0,
+        joined:''
+      }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = initialState;
+  }
+
+    loadUser = (data) => {
+      this.setState({user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+      }})
+
+  }
+
+/*  componentDidMount() {
+    fetch('http://localhost:3001/')
+    .then(response => response.json())
+    .then(console.log)
+  }*/
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+        leftCol: clarifaiFace.left_col * width,
+        topRow: clarifaiFace.top_row * height,
+        rightCol: width - (clarifaiFace.right_col * width),
+        bottomRow: height - (clarifaiFace.bottom_row * height),
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box)
+    this.setState({box:box})
+  }
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+      fetch('https://vp-smart-brain-api.herokuapp.com/imageurl',{
+          method: 'post',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            input: this.state.input
+          })
+      })
+      .then(response => response.json())
+      .then(response => {
+        //console.log('hi', response)
+        if (response) {
+          fetch('https://vp-smart-brain-api.herokuapp.com/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count}))
+            })
+            .catch(console.log)
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
+      .catch(err => console.log(err));
+  }
+
+  onRouteChange = (route) =>{
+    if (route === 'signout'){
+      this.setState(initialState)
+    }else if (route === 'home'){
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route})
+  }
+
+  render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
+    return (
+       <div className="App">
+          <Particles className="particles"
+            params={particlesOptions}
+          />
+          <Navigation isSignedIn = {isSignedIn} onRouteChange = {this.onRouteChange}/>
+          { route === 'home'
+            ? <div>
+              <Logo />
+              <Rank name = {this.state.user.name} entries = { this.state.user.entries }/>
+              <ImageLinkForm onInputChange = {this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+              <FaceRecognition box={box} imageUrl={imageUrl}/>
+              </div>
+            : ( this.state.route === 'signin' 
+              ? <SignIn loadUser = {this.loadUser} onRouteChange={this.onRouteChange}/>
+              : <Register loadUser = {this.loadUser} onRouteChange={this.onRouteChange}/>
+              )
+           }
+       </div>
+      );
+    }
+  }
 export default App;
---
+++ src/Components/Logo/Logo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import brain from './brain.png';
+import './Logo.css';
+
+const Logo = () => {
+	return (
+		<div className='ma4 mt0'>
+			<Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 100, width: 100 }} >
+ 				<div className="Tilt-inner">
+ 				 	<img src={brain} alt="Brain" style={{paddingTop:'10px'}}/>
+ 				</div>
+			</Tilt>
+		</div>
+		);
+}
+
+export default Logo;
